a9fb942e1725aa1680d956fa34077bd5296660bb
Matěj Sháněl <Tamy.denet@gmail.com>
2024-10-20 17:43:25 +0200

    Add extension method for combining simple collection with a complex one

---
+++ Core.Tests/Extensions/EnumerableExtensions/CombineSimpleWithComplexTests.cs
@@ -0,0 +1,57 @@
+﻿using NUnit.Framework;
+using Shanemat.DotNetUtils.Core.Extensions;
+
+namespace Shanemat.DotNetUtils.Core.Tests.Extensions.EnumerableExtensions;
+
+/// <summary>
+/// Contains tests for <see cref="EnumerableExtensions.CombineWith{T}(System.Collections.Generic.IEnumerable{T}?,System.Collections.Generic.IReadOnlyCollection{T[]}?)"/>
+/// </summary>
+internal sealed class CombineSimpleWithComplexTests
+{
+	#region Tests
+
+	[Test]
+	public void ShouldReturnNullWhenBothCollectionsAreNull()
+	{
+		int[]? simpleNullCollection = null;
+		int[][]? complexNullCollection = null;
+
+		Assert.That( simpleNullCollection.CombineWith( complexNullCollection ), Is.Null );
+	}
+
+	[Test]
+	public void ShouldReturnTheContentOfOtherCollectionWhenOneIsNull()
+	{
+		int[]? simpleNullCollection = null;
+		int[][]? complexNullCollection = null;
+
+		int[] simpleValues = [-75, 0, 128];
+		int[][] expectedSimpleResult = [[-75], [0], [128]];
+
+		int[][] complexValues = [[15, -2], [-39, 0]];
+		int[][] expectedComplexResult = [[15, -2], [-39, 0]];
+
+		Assert.Multiple( () =>
+		{
+			Assert.That( simpleNullCollection.CombineWith( complexValues ), Is.EquivalentTo( expectedComplexResult ) );
+			Assert.That( simpleValues.CombineWith( complexNullCollection ), Is.EquivalentTo( expectedSimpleResult ) );
+		} );
+	}
+
+	[Test]
+	public void ShouldReturnCorrectResultWhenBothCollectionsAreNotNull()
+	{
+		int[] simpleValues = [-75, 0, 128];
+		int[][] complexValues = [[15, -2], [-39, 0]];
+
+		int[][] expectedResult =
+		[
+			[-75, 15, -2], [0, 15, -2], [128, 15, -2],
+			[-75, -39, 0], [0, -39, 0], [128, -39, 0],
+		];
+
+		Assert.That( simpleValues.CombineWith( complexValues ), Is.EquivalentTo( expectedResult ) );
+	}
+
+	#endregion
+}
--- Core/Extensions/EnumerableExtensions.cs
+++ Core/Extensions/EnumerableExtensions.cs
@@ -47,6 +47,24 @@ public static class EnumerableExtensions
 		_ => source.SelectMany( x => collection.Select( y => x.Append( y ).ToArray() ) )
 	};
 
+	/// <summary>
+	/// Combines the values of the given two collections
+	/// </summary>
+	/// <param name="source">The source collection to combine</param>
+	/// <param name="collection">The other collection to combine source collection with</param>
+	/// <typeparam name="T">The type of values stored in collections to combine</typeparam>
+	/// <returns>Returns a collection of combination products</returns>
+	/// <remarks>The result will be a collection of arrays where each item is created by prepending a value from <paramref name="source"/> to an array from <paramref name="collection"/></remarks>
+	[return: NotNullIfNotNull( nameof( source ) )]
+	[return: NotNullIfNotNull( nameof( collection ) )]
+	public static IEnumerable<T[]>? CombineWith<T>( this IEnumerable<T>? source, IReadOnlyCollection<T[]>? collection ) => (source, collection) switch
+	{
+		(null, null) => null,
+		(null, _) => collection,
+		(_, null) => source.Select( v => new[] { v } ),
+		_ => source.SelectMany( x => collection.Select( y => y.Prepend( x ).ToArray() ) )
+	};
+
 	#endregion
 
 	#endregion
