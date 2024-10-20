e734876b516bee040280ca44a551772752990593
Matěj Sháněl <Tamy.denet@gmail.com>
2024-10-20 21:51:36 +0200

    Add extension method for checking whether double value is positive

---
+++ Core.Tests/Extensions/DoubleCompareExtensions/IsPositiveNullableTests.cs
@@ -0,0 +1,101 @@
+﻿using NUnit.Framework;
+using Shanemat.DotNetUtils.Core.Extensions;
+
+namespace Shanemat.DotNetUtils.Core.Tests.Extensions.DoubleCompareExtensions;
+
+/// <summary>
+/// Contains tests for <see cref="DoubleCompareExtensions.IsPositive(double?,double)"/>
+/// </summary>
+internal sealed class IsPositiveNullableTests
+{
+	#region Sources
+
+	private static IReadOnlyCollection<double> PositiveValues => Sources.PositiveValues;
+
+	private static IReadOnlyCollection<double> NegativeValues => Sources.NegativeValues;
+
+	private static IReadOnlyCollection<double> Tolerances => Sources.Tolerances;
+
+	#endregion
+
+	#region Tests
+
+	[Test]
+	public void ShouldThrowArgumentExceptionWhenToleranceIsNegative()
+	{
+		double? value = 0.0;
+
+		Assert.Throws<ArgumentException>( () => value.IsPositive( -1.0 ) );
+	}
+
+	[Test]
+	public void ShouldHandleNullValue()
+	{
+		double? nullValue = null;
+
+		Assert.That( nullValue.IsPositive(), Is.False );
+	}
+
+	[Test]
+	[TestCaseSource( nameof( PositiveValues ) )]
+	public void ShouldReturnTrueWhenValueIsPositive( double? value )
+	{
+		Assert.That( value.IsPositive(), Is.True );
+	}
+
+	[Test]
+	[TestCaseSource( nameof( NegativeValues ) )]
+	public void ShouldReturnFalseWhenValueIsNegative( double? value )
+	{
+		Assert.That( value.IsPositive(), Is.False );
+	}
+
+	[Test]
+	[TestCaseSource( nameof( Tolerances ) )]
+	public void ShouldReturnTrueWhenValueIsPositiveOutsideTolerance( double tolerance )
+	{
+		double? value = tolerance * 1.5;
+
+		Assert.That( value.IsPositive( tolerance ), Is.True );
+	}
+
+	[Test]
+	[TestCaseSource( nameof( Tolerances ) )]
+	public void ShouldReturnFalseWhenValueIsPositiveWithinTolerance( double tolerance )
+	{
+		double? values = tolerance * 0.5;
+
+		Assert.That( values.IsPositive( tolerance ), Is.False );
+	}
+
+	[Test]
+	public void ShouldReturnFalseWhenValueIsZero()
+	{
+		double? value = 0.0;
+
+		Assert.That( value.IsPositive(), Is.False );
+	}
+
+	[Test]
+	public void ShouldWorkWithInfinities()
+	{
+		double? negativeInfinity = double.NegativeInfinity;
+		double? positiveInfinity = double.PositiveInfinity;
+
+		Assert.Multiple( () =>
+		{
+			Assert.That( negativeInfinity.IsPositive(), Is.False );
+			Assert.That( positiveInfinity.IsPositive(), Is.True );
+		} );
+	}
+
+	[Test]
+	public void ShouldReturnFalseForInvalidNumber()
+	{
+		double? invalidNumber = double.NaN;
+
+		Assert.That( invalidNumber.IsPositive(), Is.False );
+	}
+
+	#endregion
+}
---
+++ Core.Tests/Extensions/DoubleCompareExtensions/IsPositiveTests.cs
@@ -0,0 +1,86 @@
+﻿using NUnit.Framework;
+using Shanemat.DotNetUtils.Core.Extensions;
+
+namespace Shanemat.DotNetUtils.Core.Tests.Extensions.DoubleCompareExtensions;
+
+/// <summary>
+/// Contains tests for <see cref="DoubleCompareExtensions.IsPositive(double,double)"/>
+/// </summary>
+internal sealed class IsPositiveTests
+{
+	#region Sources
+
+	private static IReadOnlyCollection<double> PositiveValues => Sources.PositiveValues;
+
+	private static IReadOnlyCollection<double> NegativeValues => Sources.NegativeValues;
+
+	private static IReadOnlyCollection<double> Tolerances => Sources.Tolerances;
+
+	#endregion
+
+	#region Tests
+
+	[Test]
+	public void ShouldThrowArgumentExceptionWhenToleranceIsNegative()
+	{
+		const double value = 0.0;
+
+		Assert.Throws<ArgumentException>( () => value.IsPositive( -1.0 ) );
+	}
+
+	[Test]
+	[TestCaseSource( nameof( PositiveValues ) )]
+	public void ShouldReturnTrueWhenValueIsPositive( double value )
+	{
+		Assert.That( value.IsPositive(), Is.True );
+	}
+
+	[Test]
+	[TestCaseSource( nameof( NegativeValues ) )]
+	public void ShouldReturnFalseWhenValueIsNegative( double value )
+	{
+		Assert.That( value.IsPositive(), Is.False );
+	}
+
+	[Test]
+	[TestCaseSource( nameof( Tolerances ) )]
+	public void ShouldReturnTrueWhenValueIsPositiveOutsideTolerance( double tolerance )
+	{
+		var value = tolerance * 1.5;
+
+		Assert.That( value.IsPositive( tolerance ), Is.True );
+	}
+
+	[Test]
+	[TestCaseSource( nameof( Tolerances ) )]
+	public void ShouldReturnFalseWhenValueIsPositiveWithinTolerance( double tolerance )
+	{
+		var values = tolerance * 0.5;
+
+		Assert.That( values.IsPositive( tolerance ), Is.False );
+	}
+
+	[Test]
+	public void ShouldReturnFalseWhenValueIsZero()
+	{
+		Assert.That( 0.0.IsPositive(), Is.False );
+	}
+
+	[Test]
+	public void ShouldWorkWithInfinities()
+	{
+		Assert.Multiple( () =>
+		{
+			Assert.That( double.NegativeInfinity.IsPositive(), Is.False );
+			Assert.That( double.PositiveInfinity.IsPositive(), Is.True );
+		} );
+	}
+
+	[Test]
+	public void ShouldReturnFalseForInvalidNumber()
+	{
+		Assert.That( double.NaN.IsPositive(), Is.False );
+	}
+
+	#endregion
+}
