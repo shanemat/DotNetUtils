e8dfe612cdc8090e8bd6fb7ca4bbabc1cc5477d0
Matěj Sháněl <Tamy.denet@gmail.com>
2024-10-20 18:16:40 +0200

    Add extension method for comparing equality of two nullable structs

---
+++ Core.Tests/Extensions/NullableExtensions/IsEqualToStructTests.cs
@@ -0,0 +1,74 @@
+﻿using NUnit.Framework;
+using Shanemat.DotNetUtils.Core.Extensions;
+
+namespace Shanemat.DotNetUtils.Core.Tests.Extensions.NullableExtensions;
+
+/// <summary>
+/// Contains tests for <see cref="NullableExtensions.IsEqualTo{T}(System.Nullable{T},System.Nullable{T},System.Func{T,T,bool})"/>
+/// </summary>
+internal sealed class IsEqualToStructTests
+{
+	#region Sources
+
+	private static IReadOnlyCollection<int> Values { get; } = [-75, 0, 128];
+
+	private static IReadOnlyCollection<Func<int, int, bool>> Comparers { get; } =
+	[
+		AreEqual,
+		AreNotEqual,
+		IsEitherValueZero,
+	];
+
+	private static IEnumerable<object[]> ValuesAndComparers => Values.Cast<object>().CombineWith( Comparers );
+
+	#endregion
+
+	#region Tests
+
+	[Test]
+	public void ShouldReturnTrueWhenBothValuesAreNull()
+	{
+		int? nullValue = null;
+
+		Assert.That( nullValue.IsEqualTo( nullValue, AreEqual ), Is.True );
+	}
+
+	[Test]
+	[TestCaseSource( nameof( Values ) )]
+	public void ShouldReturnFalseWhenOnlyOneValueIsNull( int value )
+	{
+		int? nullValue = null;
+
+		Assert.Multiple( () =>
+		{
+			Assert.That( value.IsEqualTo( nullValue, AreEqual ), Is.False );
+			Assert.That( nullValue.IsEqualTo( value, AreEqual ), Is.False );
+		} );
+	}
+
+	[Test]
+	[TestCaseSource( nameof( Values ) )]
+	public void ShouldReturnCorrectValueWhenBothValuesAreNotNull( int value )
+	{
+		Assert.That( value.IsEqualTo( value, AreEqual ), Is.True );
+	}
+
+	[Test]
+	[TestCaseSource( nameof( ValuesAndComparers ) )]
+	public void ShouldUseTheProvidedComparer( int value, Func<int, int, bool> comparer )
+	{
+		Assert.That( value.IsEqualTo( value, comparer ), Is.EqualTo( comparer.Invoke( value, value ) ) );
+	}
+
+	#endregion
+
+	#region Methods
+
+	private static bool AreEqual( int one, int other ) => one == other;
+
+	private static bool AreNotEqual( int one, int other ) => one != other;
+
+	private static bool IsEitherValueZero( int one, int other ) => one == 0 || other == 0;
+
+	#endregion
+}
---
+++ Core/Extensions/NullableExtensions.cs
@@ -0,0 +1,54 @@
+﻿namespace Shanemat.DotNetUtils.Core.Extensions;
+
+/// <summary>
+/// Contains extension methods for nullable objects
+/// </summary>
+public static class NullableExtensions
+{
+	#region Methods
+
+	#region IsEqualTo<T>
+
+	/// <summary>
+	/// Returns a value indicating whether the given two values are equal
+	/// </summary>
+	/// <param name="one">One of the values to compare</param>
+	/// <param name="other">The other value to compare</param>
+	/// <param name="equalityComparer">The function to use for comparing two non-null values</param>
+	/// <typeparam name="T">The type of the values to compare</typeparam>
+	public static bool IsEqualTo<T>( this T? one, T? other, Func<T, T, bool> equalityComparer )
+		where T : struct
+		=> (one, other) switch
+		{
+			(null, null) => true,
+			(null, _)
+				or (_, null) => false,
+			_ => equalityComparer.Invoke( one.Value, other.Value )
+		};
+
+	/// <summary>
+	/// Returns a value indicating whether the given two values are equal
+	/// </summary>
+	/// <param name="one">One of the values to compare</param>
+	/// <param name="other">The other value to compare</param>
+	/// <param name="equalityComparer">The function to use for comparing two non-null values</param>
+	/// <typeparam name="T">The type of the values to compare</typeparam>
+	public static bool IsEqualTo<T>( this T one, T? other, Func<T, T, bool> equalityComparer )
+		where T : struct
+		=> IsEqualTo( (T?) one, other, equalityComparer );
+
+	/// <summary>
+	/// Returns a value indicating whether the given two values are equal
+	/// </summary>
+	/// <param name="one">One of the values to compare</param>
+	/// <param name="other">The other value to compare</param>
+	/// <param name="equalityComparer">The function to use for comparing two non-null values</param>
+	/// <typeparam name="T">The type of the values to compare</typeparam>
+	public static bool IsEqualTo<T>( this T? one, T other, Func<T, T, bool> equalityComparer )
+		where T : struct
+		=> IsEqualTo( one, (T?) other, equalityComparer );
+
+	#endregion
+
+	#endregion
+}
