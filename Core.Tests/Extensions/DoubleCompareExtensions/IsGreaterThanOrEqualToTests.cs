fe348d797fca91d04deaf144b47e08aa51731e42
Matěj Sháněl <Tamy.denet@gmail.com>
2024-10-20 21:01:55 +0200

    Add extension method for checking whether one double value is greater than or equal to another

---
+++ Core.Tests/Extensions/DoubleCompareExtensions/IsGreaterThanOrEqualToTests.cs
@@ -0,0 +1,69 @@
+﻿using NUnit.Framework;
+using Shanemat.DotNetUtils.Core.Extensions;
+
+namespace Shanemat.DotNetUtils.Core.Tests.Extensions.DoubleCompareExtensions;
+
+/// <summary>
+/// Contains tests for <see cref="DoubleCompareExtensions.IsGreaterThanOrEqualTo(double,double,double)"/>
+/// </summary>
+internal sealed class IsGreaterThanOrEqualToTests
+{
+	#region Sources
+
+	private static IReadOnlyCollection<double> Values => Sources.Values;
+
+	private static IEnumerable<double[]> ValuesAndTolerances => Values.CombineWith( Sources.Tolerances );
+
+	#endregion
+
+	#region Tests
+
+	[Test]
+	public void ShouldThrowArgumentExceptionWhenToleranceIsNegative()
+	{
+		const double value = 0.0;
+
+		Assert.Throws<ArgumentException>( () => value.IsGreaterThanOrEqualTo( value, -1.0 ) );
+	}
+
+	[Test]
+	[TestCaseSource( nameof( Values ) )]
+	public void ShouldBeReflexive( double value )
+	{
+		Assert.That( value.IsGreaterThanOrEqualTo( value ), Is.True );
+	}
+
+	[Test]
+	[TestCaseSource( nameof( ValuesAndTolerances ) )]
+	public void ShouldReturnTrueWhenFirstValueIsLessThanSecondWithinTolerance( double value, double tolerance )
+	{
+		Assert.That( value.IsGreaterThanOrEqualTo( value + tolerance * 0.5, tolerance ), Is.True );
+	}
+
+	[Test]
+	[TestCaseSource( nameof( ValuesAndTolerances ) )]
+	public void ShouldReturnFalseWhenFirstValueIsLessThanSecondOutsideTolerance( double value, double tolerance )
+	{
+		Assert.That( value.IsGreaterThanOrEqualTo( value + tolerance * 1.5, tolerance ), Is.False );
+	}
+
+	[Test]
+	[TestCaseSource( nameof( Values ) )]
+	public void ShouldWorkWithInfinities( double value )
+	{
+		Assert.Multiple( () =>
+		{
+			Assert.That( value.IsGreaterThanOrEqualTo( double.NegativeInfinity ), Is.True );
+			Assert.That( value.IsGreaterThanOrEqualTo( double.PositiveInfinity ), Is.False );
+		} );
+	}
+
+	[Test]
+	[TestCaseSource( nameof( Values ) )]
+	public void ShouldReturnFalseForInvalidNumber( double value )
+	{
+		Assert.That( value.IsGreaterThanOrEqualTo( double.NaN ), Is.False );
+	}
+
+	#endregion
+}
--- Core/Extensions/DoubleCompareExtensions.cs
+++ Core/Extensions/DoubleCompareExtensions.cs
@@ -82,6 +82,39 @@ public static class DoubleCompareExtensions
 
 	#endregion
 
+	#region IsGreaterThanOrEqualTo
+
+	/// <summary>
+	/// Returns a value indicating whether the first value is greater than or equal to the second one (within specified tolerance)
+	/// </summary>
+	/// <param name="value">The value to compare</param>
+	/// <param name="otherValue">The value to compare the first value to</param>
+	/// <param name="tolerance">The tolerance to use</param>
+	/// <exception cref="ArgumentException">Thrown in case supplied tolerance is negative</exception>
+	public static bool IsGreaterThanOrEqualTo( this double value, double otherValue, double tolerance = StandardEpsilon )
+	{
+		CheckTolerance( tolerance );
+
+		return value - otherValue >= -tolerance;
+	}
+
+	/// <summary>
+	/// Returns a value indicating whether the first value is greater than or equal to the second one (within specified tolerance)
+	/// </summary>
+	/// <param name="value">The value to compare</param>
+	/// <param name="otherValue">The value to compare the first value to</param>
+	/// <param name="tolerance">The tolerance to use</param>
+	/// <exception cref="ArgumentException">Thrown in case supplied tolerance is negative</exception>
+	/// <remarks>Please note that this method will return <see langword="false"/> if either (or both) of the values is <see langword="null"/></remarks>
+	public static bool IsGreaterThanOrEqualTo( this double? value, double? otherValue, double tolerance = StandardEpsilon )
+	{
+		CheckTolerance( tolerance );
+
+		return value - otherValue >= -tolerance;
+	}
+
+	#endregion
+
 	#region Helpers
 
 	/// <summary>
