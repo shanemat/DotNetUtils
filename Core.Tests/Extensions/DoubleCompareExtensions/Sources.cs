0d62b64b87bec2e0385915035eb301c529fbb975
Matěj Sháněl <Tamy.denet@gmail.com>
2024-10-20 18:27:14 +0200

    Add source values for double comparison tests

---
+++ Core.Tests/Extensions/DoubleCompareExtensions/Sources.cs
@@ -0,0 +1,21 @@
+﻿namespace Shanemat.DotNetUtils.Core.Tests.Extensions.DoubleCompareExtensions;
+
+/// <summary>
+/// Contains the source values to use in double comparison tests
+/// </summary>
+internal static class Sources
+{
+	#region Properties
+
+	internal static IReadOnlyCollection<double> PositiveValues { get; } = [2.54785956, 5986.126478, Math.E];
+
+	internal static IReadOnlyCollection<double> NegativeValues { get; } = [-250.349745, -1.515687312, -Math.PI];
+
+	internal static IReadOnlyCollection<double> SpecialValues { get; } = [double.NaN, double.PositiveInfinity, double.NegativeInfinity];
+
+	internal static IReadOnlyCollection<double> Tolerances { get; } = [1e-3, 1e-12];
+
+	internal static IReadOnlyCollection<double> Values { get; } = NegativeValues.Concat( PositiveValues ).ToArray();
+
+	#endregion
+}
