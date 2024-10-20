228d178d56e92d1f709b96dc950794fc97435cac
Matěj Sháněl <Tamy.denet@gmail.com>
2024-10-20 18:17:05 +0200

    Add extension method for comparing equality of two nullable classes

---
+++ Core.Tests/Extensions/NullableExtensions/IsEqualToClassTests.cs
@@ -0,0 +1,85 @@
+﻿using NUnit.Framework;
+using Shanemat.DotNetUtils.Core.Extensions;
+
+namespace Shanemat.DotNetUtils.Core.Tests.Extensions.NullableExtensions;
+
+/// <summary>
+/// Contains tests for <see cref="NullableExtensions.IsEqualTo{T}(T,T,System.Func{T,T,bool})"/>
+/// </summary>
+internal sealed class IsEqualToClassTests
+{
+	#region Nested Types
+
+	internal record Record( int Value );
+
+	#endregion
+
+	#region Sources
+
+	private static IReadOnlyCollection<int> Values { get; } = [-75, 0, 128];
+
+	private static IReadOnlyCollection<Func<Record, Record, bool>> Comparers { get; } =
+	[
+		AreEqual,
+		AreNotEqual,
+		HasEitherZeroValue,
+	];
+
+	private static IEnumerable<object[]> ValuesAndComparers => Values.Cast<object>().CombineWith( Comparers );
+
+	#endregion
+
+	#region Tests
+
+	[Test]
+	public void ShouldReturnTrueWhenBothValuesAreNull()
+	{
+		Record? nullRecord = null;
+
+		Assert.That( nullRecord.IsEqualTo( nullRecord, AreEqual ), Is.True );
+	}
+
+	[Test]
+	[TestCaseSource( nameof( Values ) )]
+	public void ShouldReturnFalseWhenOnlyOneValueIsNull( int value )
+	{
+		var record = new Record( value );
+		Record? nullRecord = null;
+
+		Assert.Multiple( () =>
+		{
+			Assert.That( record.IsEqualTo( nullRecord, AreEqual ), Is.False );
+			Assert.That( nullRecord.IsEqualTo( record, AreEqual ), Is.False );
+		} );
+	}
+
+	[Test]
+	[TestCaseSource( nameof( Values ) )]
+	public void ShouldReturnCorrectValueWhenBothValuesAreNotNull( int value )
+	{
+		var record = new Record( value );
+
+		Assert.That( record.IsEqualTo( record, AreEqual ), Is.True );
+	}
+
+	[Test]
+	[TestCaseSource( nameof( ValuesAndComparers ) )]
+	public void ShouldUseTheProvidedComparer( int value, Func<Record, Record, bool> comparer )
+	{
+		var record = new Record( value );
+
+		Assert.That( record.IsEqualTo( record, comparer ), Is.EqualTo( comparer.Invoke( record, record ) ) );
+	}
+
+	#endregion
+
+	#region Methods
+
+	private static bool AreEqual( Record one, Record other ) => one.Value == other.Value;
+
+	private static bool AreNotEqual( Record one, Record other ) => one.Value != other.Value;
+
+	private static bool HasEitherZeroValue( Record one, Record other ) => one.Value == 0 || other.Value == 0;
+
+	#endregion
+}
--- Core/Extensions/NullableExtensions.cs
+++ Core/Extensions/NullableExtensions.cs
@@ -48,6 +48,23 @@ public static class NullableExtensions
 		where T : struct
 		=> IsEqualTo( one, (T?) other, equalityComparer );
 
+	/// <summary>
+	/// Returns a value indicating whether the given two values are equal
+	/// </summary>
+	/// <param name="one">One of the values to compare</param>
+	/// <param name="other">The other value to compare</param>
+	/// <param name="equalityComparer">The function to use for comparing two non-null values</param>
+	/// <typeparam name="T">The type of the values to compare</typeparam>
+	public static bool IsEqualTo<T>( this T? one, T? other, Func<T, T, bool> equalityComparer )
+		where T : class
+		=> (one, other) switch
+		{
+			(null, null) => true,
+			(null, _)
+				or (_, null) => false,
+			_ => equalityComparer.Invoke( one, other )
+		};
+
 	#endregion
 
 	#endregion
