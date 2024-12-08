0f6c18f8a443ce963724fff19c3a92d3fe1f53be
Matěj Sháněl <Tamy.denet@gmail.com>
2024-12-08 15:35:44 +0100

    Add source values for command tests

---
+++ Wpf.Tests/Commands/Sources.cs
@@ -0,0 +1,13 @@
+﻿namespace Shanemat.DotNetUtils.Wpf.Tests.Commands;
+
+/// <summary>
+/// Contains the source values to use in command tests
+/// </summary>
+internal static class Sources
+{
+	#region Properties
+
+	internal static IEnumerable<int?> Values => [-57, 0, 49, null];
+
+	#endregion
+}
