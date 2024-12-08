9c9d3a83fb8a0ea1ef62004d70dadac69e012e25
Matěj Sháněl <Tamy.denet@gmail.com>
2024-12-08 14:32:25 +0100

    Add support for the fire and forget pattern for tasks

---
+++ Core.Tests/Extensions/TaskExtensions/FireAndForgetTests.cs
@@ -0,0 +1,48 @@
+﻿using NUnit.Framework;
+using Shanemat.DotNetUtils.Core.Extensions;
+
+namespace Shanemat.DotNetUtils.Core.Tests.Extensions.TaskExtensions;
+
+/// <summary>
+/// Contains tests for <see cref="Core.Extensions.TaskExtensions.FireAndForget"/> method
+/// </summary>
+internal sealed class FireAndForgetTests
+{
+	#region Tests
+
+	[Test]
+	public void ShouldHandleNullTask()
+	{
+		Task? task = null;
+
+		Assert.DoesNotThrow( () => task.FireAndForget() );
+	}
+
+	[Test]
+	public void ShouldIgnoreInnerExceptions()
+	{
+		Assert.DoesNotThrow( () => Task.Run( () => throw new Exception() ).FireAndForget() );
+	}
+
+	[Test]
+	public async Task ShouldUseExceptionHandler()
+	{
+		var flag = false;
+		var taskCompletionSource = new TaskCompletionSource();
+
+		Assert.DoesNotThrow( () => Task.Run( () => throw new Exception() ).FireAndForget( onExceptionCaught: SetFlag ) );
+
+		await taskCompletionSource.Task;
+
+		Assert.That( flag, Is.True );
+
+		void SetFlag( Exception e )
+		{
+			flag = true;
+
+			taskCompletionSource.SetResult();
+		}
+	}
+
+	#endregion
+}
---
+++ Core/Extensions/TaskExtensions.cs
@@ -0,0 +1,34 @@
+﻿namespace Shanemat.DotNetUtils.Core.Extensions;
+
+/// <summary>
+/// Contains extension methods for <see cref="Task"/> class
+/// </summary>
+public static class TaskExtensions
+{
+	#region Methods
+
+	/// <summary>
+	/// Allows for using the task in a synchronous context
+	/// </summary>
+	/// <param name="task">The task to be executed</param>
+	/// <param name="continueOnCapturedContext">A value indicating whether an attempt to continue on captured context should be made</param>
+	/// <param name="onExceptionCaught">The action to execute if an exception occurs during execution of this task</param>
+	[System.Diagnostics.CodeAnalysis.SuppressMessage( "Major Bug", "S3168:\"async\" methods should not return \"void\"",
+		Justification = "Here it is necessary to implement the \"Fire and Forget\" task handling" )]
+	public static async void FireAndForget( this Task? task, bool continueOnCapturedContext = false, Action<Exception>? onExceptionCaught = null )
+	{
+		if( task is null )
+			return;
+
+		try
+		{
+			await task.ConfigureAwait( continueOnCapturedContext );
+		}
+		catch( Exception exception )
+		{
+			onExceptionCaught?.Invoke( exception );
+		}
+	}
+
+	#endregion
+}
