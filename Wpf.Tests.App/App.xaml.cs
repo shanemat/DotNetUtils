f653a05049777a28b3a394de3516d9af15262ffd
Matěj Sháněl <Tamy.denet@gmail.com>
2024-12-08 09:12:35 +0100

    Initialize the Wpf.Tests.App project

---
+++ Wpf.Tests.App/App.xaml.cs
@@ -0,0 +1,6 @@
+﻿namespace Wpf.Tests.App;
+
+/// <summary>
+/// Represents a view for the application
+/// </summary>
+public partial class App;
---
+++ Wpf.Tests.App/MainWindow.xaml.cs
@@ -0,0 +1,19 @@
+﻿namespace Wpf.Tests.App;
+
+/// <summary>
+/// Represents a view for the main window
+/// </summary>
+public partial class MainWindow
+{
+	#region Constructors
+
+	/// <summary>
+	/// Creates a new instance of <see cref="MainWindow"/> class
+	/// </summary>
+	public MainWindow()
+	{
+		InitializeComponent();
+	}
+
+	#endregion
+}
